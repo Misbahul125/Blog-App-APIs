05cb09c3526dd7f1411c9aea0aabd4582a4e7c09
Misbahul Haque <misbahulhaque2001@gmail.com>
2023-01-13 22:21:39 +0530

    Initial Commit

---
+++ Blog-App-APIs/src/main/java/com/codershub/blog/BlogAppApIsApplication.java
@@ -0,0 +1,13 @@
+package com.codershub.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppApIsApplication.class, args);
+	}
+
+}
